5ca45eb95bb4a34c185cda771c1b981d4e4a4dea
Jake Maschoff <maschoffjake@gmail.com>
2020-04-27 10:42:47 -0600

    update.

--- oauth2/oauth2.py
+++ oauth2/oauth2.py
@@ -1,31 +1,51 @@
-from __future__ import print_function
 import os.path
+import sys
+import pprint
+
+# Used for OAuth2
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+# Request access to reading data from google drive
+# https://developers.google.com/drive/api/v3/about-auth (for other scopes we could use)
+SCOPES = ['https://www.googleapis.com/auth/drive']
+# SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+pp = pprint.PrettyPrinter(indent=4)
+refresh_token = ''
 
 def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
+
+    # Build the credentials associated with our APP (google app)
     flow = InstalledAppFlow.from_client_secrets_file(
         '../conf/credentials.json', SCOPES)
     creds = flow.run_local_server(port=0)
 
-    service = build('gmail', 'v1', credentials=creds)
+    print(creds)
+    pp.pprint(vars(creds))
+    drive = build('drive', 'v3', credentials=creds)
+    files = drive.files().list().execute()
+    # service = build('gmail', 'v1', credentials=creds) Gmail
 
+    print("Verified!")
+    # files_arr = files['files']
+    # for item in files_arr:
+    #     print(item['name'])
+    # pp.pprint(files)
     # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
+    # results = service.users().labels().list(userId='me').execute()
+    # labels = results.get('labels', [])
 
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+    # print("Size of results:", sys.getsizeof(results))
+
+    # if not labels:
+    #     print('No labels found.')
+    # else:
+    #     print('Labels:')
+    #     for label in labels:
+    #         print(label['name'])
 
 if __name__ == '__main__':
     main()
