977c79ce02c2ffaf89966685cb159213540c6329
Sean <dcbird92@gmail.com>
2020-04-28 15:07:03 -0600

    Augmented EKE

--- EKE/AugmentedClient.py
+++ EKE/AugmentedClient.py
@@ -1,32 +1,38 @@
 import socket
-import json
-import random
 import sys
+import logging
+import json
+import hashlib
 from Crypto.Cipher import DES3
-from Crypto.Random import get_random_bytes
+from diffiehellman.diffiehellman import DiffieHellman
 from Interface.ProtocolClientInterface import ProtocolClientInterface
 
 BUFFER_SIZE = 4096
 
-class AugmentedClient(ProtocolClientInterface):
-    def __init__(self, host, port, password, base, modulus):
-        """
-        Set hostname, port, protocol
-        :param hostname: Hostname of this client
-        :param port: Port of this client
-        :param base: base value for discrete logarithm
-        :param modulus: modulus value for discrete logarithm
-        :param password: shared password between client and server
-        """
+def bytes_to_int(data):
+    return int.from_bytes(data, byteorder=sys.byteorder)
+
+def int_to_bytes(data, size):
+    return int.to_bytes(data, size, byteorder=sys.byteorder)
+
+def create_json(data):
+    json_data = json.dumps(data)
+    json_data = json_data.zfill(len(json_data)+(8-(len(json_data) % 8)))
+    json_data = json_data.encode()
+    return json_data
+
+
+class EKEAugmentedClient(ProtocolClientInterface):
+    def __init__(self, host, port, password):
         self.hostname = host
         self.port = port
-        self.base = base
-        self.modulus = modulus
+        self.diffie = DiffieHellman()
         self.password = password
-        self.cipher = None
-        self.random = None
+        self.public_key = None
+        self.encrypted_key = None
+        self.secret_key = None
         self.socket = None
-
+        self.data = None
 
     def connect(self):
         """
@@ -34,48 +40,79 @@ class AugmentedClient(ProtocolClientInterface):
         :return:
         """
         print("CLIENT: Connecting to server...")
+        self.create_public_key()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.host, self.port))
+        self.socket.connect((self.hostname, self.port))
         print("CLIENT: Connected to server...")
         return
 
-    
-    def handshake(self):
-        self.create_cipher()
-        rand = random.randint(0, 100)
-        RA = self.base ** rand % self.modulus
-        msg = self.cipher.encode(RA)
-        message_1 = {
+    def receive_file(self, message_size=16*1024):
+        """
+        Function used for receiving a file from the connection made, defaults to 16KB sized messages
+        :param message_size:
+        :return:
+        """
+        logging.debug('CLIENT: Beginning to receive.')
+        print("CLIENT: receiving file")
+        total_data = []
+        data = self.socket.recv(message_size)
+        while data != bytes(''.encode()):
+            total_data.append(data)
+            data = self.socket.recv(message_size)
+        print("CLIENT: received file")
+
+        logging.debug('CLIENT: Done receiving file')
+
+    def send_public_key(self):
+        if self.public_key is None:
+            self.create_public_key()
+        m = hashlib.sha1()
+        m.update(self.password)
+        x = m.digest()
+        cipher = DES3.new(x[:16], DES3.MODE_ECB, 'This is an IV')
+        encrypted = cipher.encrypt(int_to_bytes(self.public_key, BUFFER_SIZE))
+        #self.socket.sendall(encrypted)
+
+        msg = {
             "Name": "Alice",
-            "Message": int.from_bytes(msg, byteorder=sys.byteorder)
+            "Key": bytes_to_int(encrypted)
         }
-        return
+        json_msg = create_json(msg)
+        self.socket.sendall(json_msg)
+        #self.socket.sendall(int_to_bytes(self.public_key, BUFFER_SIZE))
+
+    def create_public_key(self):
+        self.diffie.generate_public_key()
+        self.public_key = self.diffie.public_key
+
+    def receive_public_key(self):
+        data = self.waiting_for_response()
+        data = data.decode()
+        data = data.lstrip("0")
+        print("CLIENT:", data)
+        msg = json.loads(data)
+
+        m = hashlib.sha1()
+        m.update(self.password)
+        x = m.digest()
+        cipher = DES3.new(x[:16], DES3.MODE_ECB, 'This is an IV')
+        encrypted = int_to_bytes(msg["Key"], BUFFER_SIZE)
+        decrypted = bytes_to_int(cipher.decrypt(encrypted))
+        if self.public_key is None:
+            self.create_public_key()
+        self.diffie.generate_shared_secret(decrypted)    
+        self.secret_key = self.diffie.shared_key
+        if self.secret_key == None:
+            print("CLIENT: secret key was not established")
+        else:
+            print("CLIENT: secret key was established")
 
 
     def waiting_for_response(self):
         while True:
-            data = self.socket.recv(BUFFER_SIZE)
+            data = self.socket.recv(BUFFER_SIZE*10)
             if not data:
                 break
             else:
                 return data
 
-
-    def create_json(self, data):
-        json_data = json.dumps(data)
-        json_data = json_data.zfill(len(json_data)+(8-(len(json_data) % 8)))
-        json_data = json_data.encode()
-        return json_data
-
-    def create_cipher(self):
-        self.cipher = DES3.new(self.password, DES3.MODE_ECB, 'This is an IV')
-
-    
-    def bytes_to_int(data):
-        return int.from_bytes(data, byteorder=sys.byteorder)
-
-    def int_to_bytes(data, size):
-        return int.to_bytes(data, size, byteorder=sys.byteorder)
-
-    def receive_file(self, message_size: int):
-        return
--- EKE/AugmentedServer.py
+++ EKE/AugmentedServer.py
@@ -1,48 +1,132 @@
 import socket
+import logging
+import sys
+import json
+import struct
+import os
+import hashlib
+from Crypto.Cipher import DES3
 from Crypto.Random import get_random_bytes
+from diffiehellman.diffiehellman import DiffieHellman
 from Interface.ProtocolServerInterface import ProtocolServerInterface
 
 BUFFER_SIZE = 4096
 
-class AugmentedServer(ProtocolServerInterface):
-    def __init__(self, host, port, base, modulus):
+
+def bytes_to_int(data):
+    return int.from_bytes(data, byteorder=sys.byteorder)
+
+def int_to_bytes(data, size):
+    return int.to_bytes(data, size, byteorder=sys.byteorder)
+
+def file_check(file):
+    if os.path.exists(file):
+        os.remove(file)
+
+def create_json(data):
+    json_data = json.dumps(data)
+    json_data = json_data.zfill(len(json_data)+(8-(len(json_data) % 8)))
+    json_data = json_data.encode()
+    return json_data
+
+
+class EKEAugmentedServer(ProtocolServerInterface):
+    def __init__(self, host, port):
         self.hostname = host
         self.port = port
-        self.base = base
-        self.modulus = modulus
-        self.connection = None
+        self.diffie = DiffieHellman()
         self.passwords = {}
-        self.password = None
+        self.public_key = None
+        self.encrypted_key = None
+        self.secret_key = None
+        self.connection = None
+
 
     def start_server(self):
+        """
+        Function used to start the SLS server
+        Once a connection is made, it creates a new thread and passes it off to a new function (handle_sls)
+        :return:
+        """
+        self.create_public_key()
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((self.hostname, self.port))
         s.listen(5)
-        ssock = context.wrap_socket(s, server_side=True)
-        print('SERVER: listening on', self.hostname, ':', self.port, '...')
 
         # Connection made
-        (conn, address) = ssock.accept()
+        (conn, address) = s.accept()
         self.connection = conn
+
         print('SERVER: Connection made with client')
-        return
 
-    def send_file(self, path_to_file: str, message_size: int):
-        return
+    def send_file(self, path_to_file, message_size=16*1024):
+        """
+        Function used for sending a file with the connection that has been made. Defaults to message sizes of 16KB
+        :param path_to_file:
+        :param message_size:
+        :return:
+        """
+        logging.debug('SERVER: Beginning to send', path_to_file)
+        print("SERVER: sending file")
+        # Going to transfer the file passed in as arg (account for \r\n w/ newline)
+        with open(path_to_file, 'r+b') as f:
+            data = f.read(message_size)
+            while data:
+                self.connection.sendall(data)
 
-    def add_password(self, name, password):
-        self.passwords[name] = password
-        return
+                # Try to read in more data to send
+                data = f.read(message_size)
+        logging.debug('SERVER: Done sending file.')
+
+
+    def send_public_key(self):
+        if self.public_key is None:
+            self.create_public_key()
+        cipher = DES3.new(self.passwords["Alice"], DES3.MODE_ECB, 'This is an IV')
+        encrypted = cipher.encrypt(int_to_bytes(self.public_key, BUFFER_SIZE))
+
+        msg = {
+            "Key": bytes_to_int(encrypted)
+        }
+        json_msg = create_json(msg)
+        print("SERVER:", json_msg)
+        self.connection.sendall(json_msg)
+
+
+    def create_public_key(self):
+        self.diffie.generate_public_key()
+        self.public_key = self.diffie.public_key
+    
+
+    def receive_public_key(self):
+        data = self.waiting_for_response()
+        data = data.decode()
+        data = data.lstrip("0")
+        msg = json.loads(data)
+
+        cipher = DES3.new(self.passwords[msg["Name"]], DES3.MODE_ECB, 'This is an IV')
+        encrypted = int_to_bytes(msg["Key"], BUFFER_SIZE)
+        decrypted = bytes_to_int(cipher.decrypt(encrypted))
+        if self.public_key is None:
+            self.create_public_key()
+        self.diffie.generate_shared_secret(decrypted)    
+        self.secret_key = self.diffie.shared_key
+        if self.secret_key == None:
+            print("SERVER: secret key was not established")
+        else:
+            print("SERVER: secret key was established")
+
 
     def waiting_for_response(self):
         while True:
-            data = self.conn.recv(BUFFER_SIZE)
+            data = self.connection.recv(BUFFER_SIZE*10)
             if not data:
                 break
             else:
                 return data
 
-    def handshake(self):
-        return
-
+    def add_password(self, name, password):
+        m = hashlib.sha1()
+        m.update(password)
+        x = m.digest()
+        self.passwords[name] = x[:16]
